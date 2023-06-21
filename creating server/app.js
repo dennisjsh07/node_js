f912467b98ab147f22b70c8121e4ea1527613eb4
dennis <dennisjshofficial@gmail.com>
2023-06-21 13:17:23 +0530

    name added to console

--- creating server/app.js
+++ creating server/app.js
@@ -1,16 +1,12 @@
 // // import the http module...
-// const http = require('http');
+const http = require('http');
 
 // // adding createsServer api...
-// const server = http.createServer((req,res)=>{
-//     console.log(req);
-// }); //we are telling the create server to execute this function everytime it receives a request.
-
-
-// server.listen(4000);
-
-const http = require('http');
-const server = http.createServer((req, res) => {
+const server = http.createServer((req,res)=>{
     console.log("dennis joshua j");
-  });
+}); //we are telling the create server to execute this function everytime it receives a request.
+
+
 server.listen(4000);
+
+
