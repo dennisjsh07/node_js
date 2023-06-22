9e06197b23b88f1cca790fbaedeb2c7cfa3c2671
dennis <dennisjshofficial@gmail.com>
2023-06-22 17:53:03 +0530

    first - commit for routes

---
+++ routes/routes.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+requestHandler = (req,res)=>{
+    let url = req.url;
+    let method = req.method;
+    if(url === '/'){
+        fs.readFile('message.txt',{encoding: 'utf-8'},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log(`data from file = ${data}`);
+            res.write('<html>');
+            res.write('<head><title>My - Form</title></head>');
+            res.write(`<body>${data}</body>`);
+            res.write('<body>');
+            res.write('<form action="/message" method="POST">');
+            res.write('<input type="text" name="message"><input type="submit">')
+            res.write('</form>');
+            res.write('</body>');
+            res.write('</html>');
+            return res.end();
+        });
+    }
+    else if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end',()=>{
+            let parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            let message = parsedBody.split('=')[1];
+            //write in file
+            fs.writeFile('message.txt',message,(err)=>{
+                if(err){
+                    console.log(err);
+                }
+                //redirect the server.
+                res.statusCode = 302;
+                res.setHeader('location','/');
+                return res.end();
+            });
+        });   
+    }
+    else{
+        res.setHeader('content-type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Hello World</title></head>');
+        res.write('<body><h1>Hello World</h1></body>');
+        res.write('</html>');
+    }
+}
+
+// exporting to app.js....
+// method - 1....
+// module.exports = requestHandler;
+
+// method - 2....
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'some hard coded text'
+// }
+
+// method - 3.....
+exports.handler = requestHandler;
+exports.someText = 'some hard coded text';
